90e72d9e43bbb12e937771f50015c593511f1fcf
Ray Jang <ajou4095@gmail.com>
2024-03-03 21:50:59 +0900

    [Feature] Login Flow QA 진행

--- presentation/src/main/kotlin/kr/hobbly/hobbyweekly/android/presentation/ui/main/nonlogin/register/hobby/RegisterHobbyScreen.kt
+++ presentation/src/main/kotlin/kr/hobbly/hobbyweekly/android/presentation/ui/main/nonlogin/register/hobby/RegisterHobbyScreen.kt
@@ -14,17 +14,24 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -34,12 +41,16 @@ import kotlinx.coroutines.plus
 import kr.hobbly.hobbyweekly.android.common.util.coroutine.event.MutableEventFlow
 import kr.hobbly.hobbyweekly.android.common.util.coroutine.event.eventObserve
 import kr.hobbly.hobbyweekly.android.presentation.common.theme.HeadlineRegular
+import kr.hobbly.hobbyweekly.android.presentation.common.theme.LabelRegular
 import kr.hobbly.hobbyweekly.android.presentation.common.theme.Neutral400
 import kr.hobbly.hobbyweekly.android.presentation.common.theme.Neutral900
 import kr.hobbly.hobbyweekly.android.presentation.common.theme.Red
 import kr.hobbly.hobbyweekly.android.presentation.common.theme.Space10
 import kr.hobbly.hobbyweekly.android.presentation.common.theme.Space12
+import kr.hobbly.hobbyweekly.android.presentation.common.theme.Space20
 import kr.hobbly.hobbyweekly.android.presentation.common.theme.Space32
+import kr.hobbly.hobbyweekly.android.presentation.common.theme.Space40
+import kr.hobbly.hobbyweekly.android.presentation.common.theme.Space44
 import kr.hobbly.hobbyweekly.android.presentation.common.theme.Space52
 import kr.hobbly.hobbyweekly.android.presentation.common.theme.Space6
 import kr.hobbly.hobbyweekly.android.presentation.common.theme.Space8
@@ -52,6 +63,7 @@ import kr.hobbly.hobbyweekly.android.presentation.common.view.confirm.ConfirmBut
 import kr.hobbly.hobbyweekly.android.presentation.common.view.confirm.ConfirmButtonProperties
 import kr.hobbly.hobbyweekly.android.presentation.common.view.confirm.ConfirmButtonSize
 import kr.hobbly.hobbyweekly.android.presentation.common.view.confirm.ConfirmButtonType
+import kr.hobbly.hobbyweekly.android.presentation.common.view.textfield.TypingTextField
 import kr.hobbly.hobbyweekly.android.presentation.ui.main.nonlogin.register.result.RegisterResultConstant
 
 @OptIn(ExperimentalLayoutApi::class)
@@ -65,12 +77,16 @@ fun RegisterHobbyScreen(
     val scope = rememberCoroutineScope() + handler
 
     val minimumHobbyCount = 5
+    val additionalHobbyList = remember { mutableStateListOf<String>() }
+    val hobbyList = data.hobbyList + additionalHobbyList
     val checkedHobbyList = remember { mutableStateListOf<String>() }
+    var customHobby: String by remember { mutableStateOf("") }
 
-    val isMinimumHobbyChecked = data.hobbyList.count { hobby ->
+    val isMinimumHobbyChecked = hobbyList.count { hobby ->
         checkedHobbyList.contains(hobby)
     } >= minimumHobbyCount
 
+    val isCustomHobbyValid = hobbyList.all { it != customHobby }
     val isConfirmButtonEnabled = state != RegisterHobbyState.Loading && isMinimumHobbyChecked
 
     fun navigateToRegisterResult() {
@@ -104,7 +120,7 @@ fun RegisterHobbyScreen(
             horizontalArrangement = Arrangement.spacedBy(Space8),
             verticalArrangement = Arrangement.spacedBy(Space8),
         ) {
-            data.hobbyList.forEach { hobby ->
+            hobbyList.forEach { hobby ->
                 RegisterHobbyScreenItem(
                     hobby = hobby,
                     isSelected = checkedHobbyList.contains(hobby),
@@ -117,6 +133,41 @@ fun RegisterHobbyScreen(
                     }
                 )
             }
+        }
+        Spacer(modifier = Modifier.height(Space20))
+        TypingTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = Space40),
+            text = customHobby,
+            hintText = "취미를 입력해주세요",
+            isError = !isCustomHobbyValid,
+
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    if (isCustomHobbyValid && customHobby.isNotBlank()) {
+                        additionalHobbyList.add(customHobby)
+                        customHobby = ""
+                    }
+                }
+            ),
+            onValueChange = {
+                customHobby = it
+            },
+        )
+        if (!isCustomHobbyValid) {
+            Spacer(modifier = Modifier.height(Space6))
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = Space44),
+                text = "이미 존재하는 취미입니다.",
+                style = LabelRegular.merge(Red),
+            )
         }
         Spacer(modifier = Modifier.height(Space80))
         ConfirmButton(
