39a3f14f9e347b80d991157a3405014605f6c58d
Ray Jang <ajou4095@gmail.com>
2024-04-13 16:56:06 +0900

    [Setting] Debug Error Message

--- data/src/main/kotlin/kr/hobbly/hobbyweekly/android/data/remote/network/environment/ErrorMessageMapper.kt
+++ data/src/main/kotlin/kr/hobbly/hobbyweekly/android/data/remote/network/environment/ErrorMessageMapper.kt
@@ -1,6 +1,7 @@
 package kr.hobbly.hobbyweekly.android.data.remote.network.environment
 
 import android.content.Context
+import android.content.pm.ApplicationInfo
 import kr.hobbly.hobbyweekly.android.data.R
 import kr.hobbly.hobbyweekly.android.domain.model.nonfeature.error.UndefinedKeyException
 import timber.log.Timber
@@ -11,6 +12,9 @@ class ErrorMessageMapper(
     fun map(
         key: String
     ): String {
+        val isDebug: Boolean =
+            (0 != context.applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE)
+
         val id = when (key) {
             KEY_INTERNAL_SERVER_ERROR -> R.string.error_internal_server_error
 
@@ -20,7 +24,11 @@ class ErrorMessageMapper(
             }
         }
 
-        return context.getString(id)
+        return if (isDebug) {
+            context.getString(id) + " ($key)"
+        } else {
+            context.getString(id)
+        }
     }
 
     companion object {
